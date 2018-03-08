257218615ea380f39b6efb1f2babd727f527f5bf
Olegiwe <Starwars700@yandex.ru>
2018-03-08 20:17:21 +0300

    A book for particles

---
+++ CBook.h
@@ -0,0 +1,27 @@
+#ifndef __CBOOK_H__
+#define __CBOOK_H__
+
+#include <fstream>
+#include <vector>
+#include <map>
+#include <string>
+#include <iterator>
+#include "RtypesCore.h"
+#include "TString.h"
+
+namespace ColliderModel{
+
+  class CBook {
+  private:
+    virtual std::vector<TString> databaseUnfold(std::string) = 0;
+  public:
+    virtual Int_t updateData(TString) = 0;
+    virtual Char_t getSeparator() const = 0;
+    virtual Int_t setSeparator(Char_t) = 0;
+    virtual Int_t defaultSeparator() = 0;
+  };
+
+
+}
+
+#endif // CBOOK_H
---
+++ CParticleBook.cpp
@@ -0,0 +1,105 @@
+#include "CParticleBook.h"
+
+namespace ColliderModel {
+
+static CParticleData __error__particle = {-1.0, 0, 0.0};
+static CParticleData __null__particle = {0.0, 0, 0.0};
+
+Char_t CParticleBook::getSeparator() const
+{
+  return separator;
+}
+
+Int_t CParticleBook::setSeparator(Char_t _ch)
+{
+  separator = _ch;
+  return 0;
+}
+
+Int_t CParticleBook::defaultSeparator()
+{
+  separator = '#';
+  return 0;
+}
+
+CParticleBook& CParticleBook::getInstance()
+{
+  static CParticleBook instance;
+  return instance;
+}
+
+CParticleBook::CParticleBook()
+{
+  separator = '#';
+}
+
+std::vector<TString> CParticleBook::databaseUnfold (std::string input_str)
+{
+  std::vector<TString> UnfoldedDatabaseParts;
+  std::string::iterator input_str_iter_fast = input_str.begin();
+  std::string::iterator input_str_iter_slow = input_str.begin();
+  while (input_str_iter_slow == input_str.end())
+  {
+    while ((input_str_iter_fast != input_str.end())&&(*input_str_iter_fast != separator))
+    {
+      ++input_str_iter_fast;
+    }
+    std::string substring(input_str_iter_slow, input_str_iter_fast);
+    TString vec_member = substring;
+    UnfoldedDatabaseParts.push_back(vec_member);
+    while ((input_str_iter_fast != input_str.end())&&(*input_str_iter_fast == separator))
+    {
+      ++input_str_iter_fast;
+    }
+    input_str_iter_slow = input_str_iter_fast;
+  }
+  return UnfoldedDatabaseParts;
+}
+
+Int_t CParticleBook::updateData(TString _filename)
+{
+  std::ifstream data_file;
+  std::string line_buffer;
+  data_file.open(_filename.Data());
+  if (!data_file.is_open())
+  {
+    return -1; //Cannot open the file
+  }
+  while(!std::getline(data_file, line_buffer))
+  {
+    std::vector<TString> temp_unfolded = databaseUnfold(line_buffer);
+    if (temp_unfolded.size() < 4)
+    {
+      data_file.close();
+      return -2; //Damaged database entries
+    }
+    CParticleData temp_particle = { temp_unfolded[1].Atof(),
+                                    temp_unfolded[2].Atoi(),
+                                    temp_unfolded[3].Atof() };
+    particleMap[ temp_unfolded[0] ] = temp_particle;
+  }
+  return 0;
+}
+
+CParticleData& CParticleBook::getData(TString _particleName)
+{
+  auto search_iter = particleMap.find(_particleName);
+  if (search_iter == particleMap.end())
+  {
+    return __null__particle;
+  }
+  else return (search_iter -> second);
+}
+
+Int_t CParticleBook::checkValidity()
+{
+  for (std::map<TString, CParticleData>::iterator check_iter = particleMap.begin();
+       check_iter != particleMap.end();
+       check_iter++)
+  {
+    if (((check_iter -> second).pMass) < 0) particleMap.erase(check_iter);
+  }
+  return 0;
+}
+
+} // For namespace
