7825fe67cc04a0a5a0e437b0f9ec99549e7b5ecf
Olegiwe <o.kuznetsov>
2018-03-23 21:05:29 +0300

    KPACUBO

---
+++ CFissionBook.cpp
@@ -0,0 +1,160 @@
+namespace ColliderModel {
+
+#include <algorithm>
+#include "CFissionBook.h"
+
+/*
+class myCrippledFunctor {
+  std::vector<TString>* p_to_arr;
+  myCrippledFunctor(std::vector<TString>* _data_ref )
+  {
+    p_to_arr = _data_ref;
+  }
+  void operator()(TString _param_str)
+  {
+    p_to_arr -> push_back(_param_str);
+  }
+};
+*/
+/*
+CFissionData::CFissionData()
+{
+  fProbability = 0;
+}
+CFissionData::CFissionData(Double_t _param)
+{
+  fProbability = _param;
+}
+
+CFissionData::CFissionData(Double_t _param, std::vector<TString> _vec)
+{
+  fProbability = _param;
+  fShatters = _vec;
+}
+*/
+Char_t CFissionBook::getSeparator() const
+{
+  return separator;
+}
+
+Int_t CFissionBook::setSeparator(Char_t _symbol)
+{
+  separator = _symbol;
+  return 0;
+}
+
+Int_t CFissionBook::defaultSeparator()
+{
+  separator = '#';
+  return 0;
+}
+
+CFissionBook& CFissionBook::getInstance()
+{
+  static CFissionBook instance;
+  return instance;
+}
+
+CFissionBook::CFissionBook()
+{
+  separator = '#';
+}
+
+std::vector<TString> CFissionBook::databaseUnfold (std::string input_str)
+{
+  std::vector<TString> UnfoldedDatabaseParts;
+  std::string::iterator input_str_iter_fast = input_str.begin();
+  std::string::iterator input_str_iter_slow = input_str.begin();
+  while (input_str_iter_slow == input_str.end())
+  {
+    while ((input_str_iter_fast != input_str.end())&&(*input_str_iter_fast != separator))
+    {
+      ++input_str_iter_fast;
+    }
+    std::string substring(input_str_iter_slow, input_str_iter_fast);
+    TString vec_member = substring;
+    UnfoldedDatabaseParts.push_back(vec_member);
+    while ((input_str_iter_fast != input_str.end())&&(*input_str_iter_fast == separator))
+    {
+      ++input_str_iter_fast;
+    }
+    input_str_iter_slow = input_str_iter_fast;
+  }
+  return UnfoldedDatabaseParts;
+}
+
+std::vector<CFissionData>& CFissionBook::getData(TString _particle)
+{
+std::map<TString, std::vector <CFissionData> >::const_iterator search_iter;
+search_iter = fissionMap.find(_particle);
+if (search_iter == fissionMap.end())
+{
+  return -1;
+}
+else return (search_iter -> second);
+}
+
+Int_t CFissionBook::updateData(TString _filename)
+{
+  std::ifstream data_file;
+  std::string line_buffer;
+  data_file.open(_filename.Data());
+  if (!data_file.is_open())
+  {
+    return -1; //Cannot open the file
+  }
+  while(!std::getline(data_file, line_buffer))
+  {
+    std::vector<TString> temp_unfolded = databaseUnfold(line_buffer);
+    if (temp_unfolded.size() < 3)
+    {
+      data_file.close();
+      return -2; //Damaged database entries
+    }
+    CFissionData temp_scenario : fProbability(temp_unfolded[1].Atof());
+    std::vector<TString>::iterator point = (temp_unfolded.begin() + 2);
+    std::copy(point,
+              temp_unfolded.end(),
+              (fissionMap[ temp_unfolded[0] ]).begin()
+              );
+
+  /*  std::for_each(point,
+                  temp_unfolded.end(),
+                  myCrippledFunctor fct( &(temp_scenario.fShatters) );
+ CFissionData temp_fission = { temp_unfolded[1].Atof(),
+                                  temp_unfolded[2].Atoi(),
+                                  temp_unfolded[3].Atof() }; */
+  //  (fissionMap[ temp_unfolded[0] ]).push_back(temp_scenario);
+  }
+  return 0;
+}
+
+
+
+Int_t checkValidity()
+{
+  std::list < std::map < TString, std::vector < CFissionData > >::iterator > /*BRUTAL!*/ blacklist;
+  Int_t counter = 0;
+  for ( std::map<TString, std::vector<CFissionData> >::iterator map_iter = fissionMap.begin();
+        map_iter < fissionMap.end();
+        map_iter++ )
+        {
+          if (
+               (((map_iter -> second)[counter]).fProbability > 1) ||
+               (((map_iter -> second)[counter]).fProbability < 0) ||
+               (((map_iter -> second)[counter]).fShatters.empty() )
+             )
+          {
+            blacklist.push_front(map_iter);
+          }
+        }
+  std::list<std::map <TString, std::vector < CFissionData > >::iterator >::iterator  blacklist_iter;
+  for (blacklist_iter == blacklist.begin(); blacklist_iter < blacklist.end(); blacklist_iter++)
+  {
+    (*blacklist_iter).erase()
+  }
+  return 0;
+}
+
+
+} //For namespace
--- CFissionBook.h
+++ CFissionBook.h
@@ -10,27 +10,30 @@ struct CFissionData
 {
   Double_t fProbability;
   std::vector<TString> fShatters;
+//  CFissionData();
+//  CFissionData(Double_t _param);
+//  CFissionData(Double_t _param, std::vector<TString> _vec);
 };
 
 class CFissionBook : public CBook
 {
 public:
-  CFissionData& getData(TString _particle);
-  Int_t updateData(TString _filename);
-  Int_t checkValidity();
-  static CFissionBook& getInstance();
-  Char_t getSeparator() const;
-  Int_t setSeparator(Char_t _ch);
-  Int_t defaultSeparator();
-  virtual ~CFissionBook();
-  
+  std::vector<CFissionData>& getData(TString _particle);//
+  Int_t updateData(TString _filename); //
+  Int_t checkValidity(); //
+  static CFissionBook& getInstance(); //
+  Char_t getSeparator() const; //
+  Int_t setSeparator(Char_t _ch); //
+  Int_t defaultSeparator(); //
+  virtual ~CFissionBook() = {}; //
+
 private:
-  CFissionBook();
-  CFissionBook(const CParticleBook&);
-  CFissionBook& operator= (const CFissionBook);
-  std::vector<TString> databaseUnfold(std::string input_str);
-  std::map<TString, CFissionData> fissionMap;
-  Char_t separator;
+  CFissionBook(); //
+  CFissionBook(const CParticleBook&); //
+  CFissionBook& operator= (const CFissionBook); //
+  std::vector<TString> databaseUnfold(std::string input_str); //
+  std::map<TString, std::vector<CFissionData> > fissionMap; //
+  Char_t separator; //
 };
 
 
